f5811934b22fccbecf55da336c9f1687f26ecd68
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-06-22 20:05:44 +1000

    Fix Get Balance issues

--- wallet.js
+++ wallet.js
@@ -1,11 +1,9 @@
 window.onload = function (){
     api = "https://api.sugarchain.org"
-    var sugaraddress = $("#addressInput").val();
-    console.log(sugaraddress)
 
     function apiCall() {
         return Promise.resolve($.ajax({
-            url: api + "/balance/sugar1qtl7u435t4jly2hdaa7hrcv5qkpvwa0spd9zzc7", //+ sugaraddress
+            url: api + "/balance/" + $("#addressInput").val(),
             dataType: 'json',
             type: 'GET'
         }))
@@ -18,7 +16,6 @@ window.onload = function (){
             var getbalance = data.result.balance
             var balance = getbalance / 100000000
             $("#getBalance").text(balance + " SUGAR")
-            console.log(sugaraddress)
         })
     }
 }
